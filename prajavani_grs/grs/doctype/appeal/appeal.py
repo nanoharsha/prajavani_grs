dd9df4cb14cc89c446704ee945be1d07687fe5c5
Sreeharsha <nanoharsha@gmail.com>
2026-03-05 18:30:30 +0530

    Update appeal.py

--- prajavani_grs/grs/doctype/appeal/appeal.py
+++ prajavani_grs/grs/doctype/appeal/appeal.py
@@ -2,6 +2,15 @@ import frappe
 from frappe.model.document import Document
 
 class Appeal(Document):
+    def before_save(self):
+        pass
+
     def after_insert(self):
         if self.linked_grievance:
             frappe.db.set_value("Grievance", self.linked_grievance, "status", "Appeal Filed")
+
+def before_save(doc, method=None):
+    doc.before_save()
+
+def after_insert(doc, method=None):
+    doc.after_insert()
